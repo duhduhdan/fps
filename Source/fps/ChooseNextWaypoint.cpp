03578c5b5978f15fa0a9f06043302e7d2c83f284
duhduhdan <loudon.dan@gmail.com>
2018-12-15 17:14:50 -0700

    remove patrolling guard; use composition over inheritance

--- Source/fps/ChooseNextWaypoint.cpp
+++ Source/fps/ChooseNextWaypoint.cpp
@@ -3,24 +3,34 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(
 	UBehaviorTreeComponent& OwnerComp,
 	uint8* NodeMemory
 ) {
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute)) return EBTNodeResult::Failed;
+
+	auto PatrolPoints = PatrolRoute->getPatrolPoints();
+
+	if (PatrolPoints.Num() <= 0) {
+		UE_LOG(LogTemp, Warning, TEXT("No patrol points found a guard"));
+
+		return EBTNodeResult::Failed;
+	}
+
+	// set next waypoint
 	auto blackBoardComp = OwnerComp.GetBlackboardComponent();
 	auto index = blackBoardComp->GetValueAsInt(indexKey.SelectedKeyName);
-
-	// get patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->patrolPointsCPP;
-
-	// set patrol points
 	blackBoardComp->SetValueAsObject(waypointKey.SelectedKeyName, PatrolPoints[index]);
+
+	// cycle waypoint
+	auto nextIndex = rand() % PatrolPoints.Num();
+	blackBoardComp->SetValueAsInt(indexKey.SelectedKeyName, nextIndex);
 	
 
 	return EBTNodeResult::Succeeded;
